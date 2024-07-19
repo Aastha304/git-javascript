1a029beab0a93a22ac68fbb93c093b0ff4587cb3
Aastha304 <149258871+Aastha304@users.noreply.github.com>
2024-07-19 17:02:37 +0530

    added day01.js file

---
+++ day01.js
@@ -0,0 +1,74 @@
+// DAY 1 task
+
+// Activity 1: variable declaration
+
+var x=10;             //task 1
+console.log(x);
+
+let y="javascript";   //task 2
+console.log(y);
+
+// Activity 2: constant declaration
+
+const z=false;        //task 3
+console.log(z);
+
+//Activity 3: data types
+
+let num=1;            //task 4
+let str="codeforjoy";
+let boolvar=false;
+let obj=['java','c++'];
+let arr=[1,2,3,4,5]
+
+console.log(typeof(num));
+console.log(typeof(str));
+console.log(typeof(boolvar));
+console.log(typeof(obj));
+console.log(typeof(arr));
+
+// Activity 4: reassigning variables
+
+let a=3;             //task 5
+console.log(a);
+a=5;
+console.log(a);
+
+// Activity 5: understanding const
+
+const consvar=4;      //task6
+//consvar=6;      ERROR
+
+// Feature request
+
+  // 1.variable type console log
+let numType= 123;
+console.log("Value: ", numType, ", Type: ", typeof numType);
+
+let strType = "a string";
+console.log("Value: ", strType, ", Type: ", typeof strType);
+
+let boolType = false;
+console.log("Value: ", boolType, ", Type: ", typeof boolType);
+
+let objType = { lang: "javascript" };
+console.log("Value: ", objType, ", Type: ", typeof objType);
+
+let arrType = [1, 2, 3];
+console.log("Value: ", arrType, ", Type: ", typeof arrType);
+
+
+// 2. Reassignment Demo
+
+let varlet = "base value";
+console.log(varlet); 
+varlet = "new value";
+console.log(varlet);
+  
+const convar = "const value";
+console.log(convar); 
+// convar = "new value"; // Error
+
+
+
+
