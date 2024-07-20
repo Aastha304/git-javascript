2c22e0c6a0f021a4fb63c521fe9dea49794e7912
Aastha304 <149258871+Aastha304@users.noreply.github.com>
2024-07-20 13:48:43 +0530

    added day02.js file

---
+++ day02.js
@@ -0,0 +1,90 @@
+// DAY 2: OPERATORS
+
+//Activity 1: arithmetic operations
+
+let num1=10;
+let num2=5;
+
+console.log("sum=",num1+num2);                //task 1  
+
+console.log("differnce=",num1-num2);          //task 2
+
+console.log("multiplication=",num1*num2);     //task 3
+
+console.log("division=",num1/num2);           //task 4
+
+console.log("remainder=",num1%num2);          //task 5
+
+//Activity 2: assignment operators
+
+num1+=5;
+console.log("new num=",num1);                 //task 6
+
+num1-=5;
+console.log("num1=",num1);                    //task 7
+
+//Activity 3: comparison operators
+
+console.log("Is num1 greater than num2: ",num1>num2);   //task8
+console.log("Is num1 smaller than num2: ",num1<num2); 
+
+console.log("Is 5 greater than 5: ",5>=5);              //task9
+console.log("Is 5 lesser than 4: ",5<=4);
+
+console.log("Is null==undefined: ",null==undefined);    //task10
+console.log("Is null===undefined: ",null===undefined);
+
+//Activity 4: logic operators
+
+console.log("5<8<7: ",5<8 && 8<7);             //task 11
+
+console.log("5<8 or 5<4 : ",5<8 || 5<4);       //task 12
+
+console.log("opposite to true is: ",!true);    //task 13
+
+//Activity 5: ternary operator
+
+num1>0?console.log("Positive number"):console.log("Negative number");
+//num1=10: output= Positive number
+
+
+// Feature Request
+
+// 1.Arithmetic operations script
+
+// let num1=10;
+// let num2=5;
+
+// console.log("sum=",num1+num2);                //task 1  
+
+// console.log("differnce=",num1-num2);          //task 2
+
+// console.log("multiplication=",num1*num2);     //task 3
+
+// console.log("division=",num1/num2);           //task 4
+
+// console.log("remainder=",num1%num2);          //task 5
+
+
+// 2.Comparison and Logic Operators script
+
+// console.log("Is num1 greater than num2: ",num1>num2);   //task8
+// console.log("Is num1 smaller than num2: ",num1<num2); 
+
+// console.log("Is 5 greater than 5: ",5>=5);              //task9
+// console.log("Is 5 lesser than 4: ",5<=4);
+
+// console.log("Is null==undefined: ",null==undefined);    //task10
+// console.log("Is null===undefined: ",null===undefined);
+
+// 3.Ternary operator script
+// num1>0?console.log("Positive number"):console.log("Negative number");
+
+
+
+
+
+
+
+
+
